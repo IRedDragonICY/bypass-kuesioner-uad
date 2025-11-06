44e3bb06e11b83c276c379fe11d41c8f7e9ce00e
Ndik <98265563+IRedDragonICY@users.noreply.github.com>
2025-11-06 11:03:38 +0700

    Create KuesionerBypass.js

---
+++ KuesionerBypass.js
@@ -0,0 +1,1 @@
+(c=$('select[name="kampus"]'))&&c.val('4').trigger('change');$('input.icheck[value="4"]').iCheck('check');$('textarea[name="saran"]').val('Sangat memuaskan.');$('.button-submit').click();
